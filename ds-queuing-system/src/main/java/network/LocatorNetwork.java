5b68bdaef22f7bb41c2a1dbb5bdbd8cee50694c0
Matteo Figini <matteo4.figini@mail.polimi.it>
2024-03-24 17:41:26 +0100

    Started LocatorNetwork implementation

---
+++ ds-queuing-system/src/main/java/network/LocatorNetwork.java
@@ -0,0 +1,42 @@
+package network;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+
+/**
+ * This class handles all the network communication by the locator component.
+ * The communication between the locator and the brokers is mediated via TCP protocol,
+ * ensuring persistent connection.
+ * TODO: how is mediated the connection between the locator and the client (if required)? TCP/UDP?
+ */
+public class LocatorNetwork implements Runnable {
+    private final String ipAddress;
+    private final int port;
+    private ServerSocket serverSocket;
+
+    // TODO: maybe, an instance to the locator's controller is required.
+
+    public LocatorNetwork (String ipAddress, int port) {
+        this.ipAddress = ipAddress;
+        this.port = port;
+    }
+
+    /**
+     * Creates a thread that constantly listens to on the {@code ServerSocket}.
+     * When a new (client or) broker asks for the connection, a new {@code } is created.
+     */
+    @Override
+    public void run() {
+        try {
+            this.serverSocket = new ServerSocket(this.port);
+            System.out.println("Locator's network running on port " + this.port + " via TCP socket connection.");
+        } catch (IOException e) {
+            System.out.println("[EXCEPTION] Unable to start the locator's server socket.");
+            System.out.println(e.getMessage());
+        }
+
+        while (!Thread.currentThread().isInterrupted()) {
+            // TODO: listen to new incoming connection
+        }
+    }
+}
