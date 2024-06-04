badca9fec6465a1057c8af9a4a3b67eb90b97f15
Matteo Figini <matteo4.figini@mail.polimi.it>
2024-06-04 22:04:44 +0200

    [CLIENT] Implemented command requests (create, append, read) in the ClientController

--- ds-queuing-system/src/main/java/client/ClientController.java
+++ ds-queuing-system/src/main/java/client/ClientController.java
@@ -1,15 +1,21 @@
 package client;
 
 import messages.Message;
+import messages.application.AppendQueueRequest;
+import messages.application.CreateQueueRequest;
+import messages.application.ReadQueueRequest;
 import messages.network.*;
 
 import java.net.Inet4Address;
 import java.net.UnknownHostException;
+import java.util.List;
 import java.util.Scanner;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class ClientController {
     private final String clientName;
@@ -80,9 +86,8 @@ public class ClientController {
                 case LEADER_DISCOVERY_RESPONSE -> {
                     LeaderDiscoveryResponse leaderDiscoveryResponse = (LeaderDiscoveryResponse) message;
                     if (leaderDiscoveryResponse.absenceOfLeader()) {
-                        final int waitingSeconds = 10;
+                        final int waitingSeconds = 20;
                         System.out.println("[INFO] No available leader now: retrying in " + waitingSeconds + " seconds...");
-
                         ScheduledExecutorService retrySendingMessage = Executors.newSingleThreadScheduledExecutor();
                         retrySendingMessage.schedule(() -> clientNetwork.sendMessage("locator", new LeaderDiscoveryRequest()), waitingSeconds, TimeUnit.SECONDS);
                     } else {
@@ -96,9 +101,57 @@ public class ClientController {
 
     /**
      * Receive in input a command from the standard input and process it, based on the format of the command.
+     * The command is then processed, according to the specific type.
      * @param command The command received from the standard input.
      */
     private void interpretCommand (String command) {
-        System.out.println(command);
+        String[] commandParts = command.split(" ");
+        if (commandParts.length < 1) {
+            return;
+        }
+        if (commandParts[0].equalsIgnoreCase("create")) interpretCreateCommand(commandParts);
+        else if (commandParts[0].equalsIgnoreCase("append")) interpretAppendCommand(commandParts);
+        else if (commandParts[0].equalsIgnoreCase("read")) interpretReadCommand(commandParts);
+        else System.out.println("[ERROR] Unknown command.");
+    }
+
+    /**
+     * Format the command string to create a new queue in the following format:
+     *      create <queue_name>
+     * Then, create the {@code CreateQueueRequest} and send the message to the broker's leader.
+     * @param commandParts Array containing the words of the command.
+     */
+    private void interpretCreateCommand (String[] commandParts) {
+        if (commandParts.length < 2) return;
+        CreateQueueRequest createQueueRequest = new CreateQueueRequest(commandParts[1]);
+        System.out.println(createQueueRequest);
+        clientNetwork.sendMessage("leader", createQueueRequest);
+    }
+
+    /**
+     * Format the command string to append some integers to a queue in the following format:
+     *      append <queue_name> <elem1> <elem2> ...
+     * Then, create the {@code AppendQueueRequest} and send the message to the broker's leader.
+     * @param commandParts Array containing the words of the command.
+     */
+    private void interpretAppendCommand (String[] commandParts) {
+        if (commandParts.length < 2) return;
+        List<Integer> elementsToAppend = IntStream.range(2, commandParts.length).mapToObj(i -> Integer.parseInt(commandParts[i])).collect(Collectors.toList());
+        AppendQueueRequest appendQueueRequest = new AppendQueueRequest(commandParts[1], elementsToAppend);
+        System.out.println(appendQueueRequest);
+        clientNetwork.sendMessage("leader", appendQueueRequest);
+    }
+
+    /**
+     * Format the command string to read new elements in a queue in the following format:
+     *      read <queue_name>
+     * Then, create the {@code ReadQueueRequest} and send the message to the broker's leader.
+     * @param commandParts Array containing the words of the command.
+     */
+    private void interpretReadCommand (String[] commandParts) {
+        if (commandParts.length < 2) return;
+        ReadQueueRequest readQueueRequest = new ReadQueueRequest(commandParts[1]);
+        System.out.println(readQueueRequest);
+        clientNetwork.sendMessage("leader", readQueueRequest);
     }
 }
